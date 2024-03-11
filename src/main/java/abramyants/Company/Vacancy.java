f0e544b3b7d76659ea68fc29e4640391b214582b
Galfest <abramyants90@mail.ru>
2024-03-11 17:43:45 +0300

    hw

---
+++ src/main/java/abramyants/Company/Vacancy.java
@@ -0,0 +1,20 @@
+package abramyants.Company;
+
+
+public class Vacancy {
+    private String companyName;
+    private double salary;
+
+    public Vacancy(String companyName, double salary) {
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
--- src/main/java/abramyants/Main.java
+++ src/main/java/abramyants/Main.java
@@ -1,8 +1,32 @@
 package abramyants;
 
+import abramyants.Company.Company;
+import abramyants.JobAgency.JobAgency;
+import abramyants.Workers.Master;
+import abramyants.Workers.Student;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        JobAgency jobAgency = new JobAgency();
+        Company geekBrains = new Company("GeekBrains", 70000, jobAgency);
+        Company google = new Company("Google", 100000, jobAgency);
+        Company yandex = new Company("Yandex", 120000, jobAgency);
+
+        Master ivanov = new Master("Ivanov");
+        Master petrov = new Master("Petrov");
+        Student sidorov = new Student("Sidorov");
+
+        jobAgency.registerObserver(ivanov);
+        jobAgency.registerObserver(petrov);
+        jobAgency.registerObserver(sidorov);
+
+        for (int i = 0; i < 2; i++){
+            geekBrains.needEmployee();
+            google.needEmployee();
+            yandex.needEmployee();
+            System.out.println();
+        }
     }
 }
